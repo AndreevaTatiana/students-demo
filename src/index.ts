dbf72b7fd92707a8052ef88c16622f32865cd1f8
AndreevaTatiana <and.tati62@gmail.com>
2022-05-16 16:15:13 +0300

    add port env

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import cors from 'cors'
 //import bodyParser from 'body-parser'
 
 const app = express()
-const port = 5001
+const port = process.env.PORT || 5001
 app.use(cors())
 app.use(express.json())
 
