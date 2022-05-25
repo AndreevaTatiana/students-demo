590f34aa7badd544fe347a76fa825736db620b22
AndreevaTatiana <and.tati62@gmail.com>
2022-05-25 12:08:00 +0300

    fix by swagger

--- src/index.ts
+++ src/index.ts
@@ -19,33 +19,60 @@ app.get('/', (req: Request, res: Response ) => {
     res.send('Hello: World')
 })
 
-app.get('/videos', (req: Request, res: Response ) => {
-    res.send(videos)
+app.get('/lesson_01/api/videos', (req: Request, res: Response ) => {
+    //res.send(videos)
+    res.status(200).send(videos)
 })
 
-app.get('/videos/:videoId', (req: Request, res: Response) => {
+app.get('/lesson_01/api/videos/:videoId', (req: Request, res: Response) => {
     const id = +req.params.videoId;
        let video = videos.find(y => y.id === id)
         if (!!video) {
-            res.send(video)
+           //res.send(video)
+            res.status(200).send(video)
         } else {
             res.send(404)
         }
 })
 
-app.post('/videos', (req: Request, res: Response) => {
-    const newVideo = {
-        id: +(new Date()),
-        title: req.body.title,
-        author: 'it-incubator.eu'
+app.post('/lesson_01/api/videos', (req: Request, res: Response) => {
+    if (!!req.body.title && req.body.title.length < 40) {
+        const newVideo = {
+            id: +(new Date()),
+            title: req.body.title,
+            //author: 'it-incubator.eu'
+            author: req.body.author
+        }
+        videos.push(newVideo)
+        res.status(201).send(newVideo)
+    } else {
+        let messageErr = {
+            "errorsMessages": [
+            {
+                "message": "string",
+                "field": "string"
+            }
+        ],
+            "resultCode": 404
+        }
+        res.status(404).send(messageErr)
     }
-    videos.push(newVideo)
-    res.status(201).send(newVideo)
+
 })
 
-app.delete('/videos/:id',(req: Request, res: Response)=>{
-    videos = videos.filter((y) => y.id !== +req.params.id)
-    res.send(204)
+app.delete('/lesson_01/api/videos/:videoId',(req: Request, res: Response)=>{
+    //videos = videos.filter((y) => y.id !== +req.params.id)
+    //res.send(204)
+    const id = +req.params.videoId;
+    let video = videos.find(y => y.id === id)
+    if (!!video) {
+        //res.send(video)
+        videos = videos.filter((y) => y.id !== +req.params.id)
+        res.send(204)
+    } else {
+        res.send(404)
+    }
+
    })
 
 app.put('/videos/:id',(req: Request, res: Response)=>{
