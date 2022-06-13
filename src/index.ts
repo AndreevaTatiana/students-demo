1a9dbb29dc1ecaa49f5800cd07a3a1e606c55097
AndreevaTatiana <and.tati62@gmail.com>
2022-06-13 16:39:31 +0300

    create  routes  and repos

--- src/index.ts
+++ src/index.ts
@@ -1,118 +1,20 @@
 import express, {Request, Response} from 'express'
 import cors from 'cors'
+import {videosRouter} from "./routes/videos-routes";
 //import bodyParser from 'body-parser'
 
 const app = express()
 const port = process.env.PORT || 5001
 app.use(cors())
 app.use(express.json())
+app.use('/videos', videosRouter)
 
-let videos = [
-    {id: 1, title: 'About JS - 01', author: 'it-incubator.eu'},
-    {id: 2, title: 'About JS - 02', author: 'it-incubator.eu'},
-    {id: 3, title: 'About JS - 03', author: 'it-incubator.eu'},
-    {id: 4, title: 'About JS - 04', author: 'it-incubator.eu'},
-    {id: 5, title: 'About JS - 05', author: 'it-incubator.eu'},
-]
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello: World!!!')
 })
 
-app.get('/videos', (req: Request, res: Response) => {
-    //res.send(videos)
-    res.status(200).send(videos)
-})
 
-app.get('/videos/:videoId', (req: Request, res: Response) => {
-    const id = +req.params.videoId;
-    if (!id) {
-        res.sendStatus(404)
-    } else {
-        let video = videos.find(y => y.id === id)
-        if (!!video) {
-            //res.send(video)
-            res.status(200).send(video)
-        } else {
-            res.sendStatus(404)
-        }
-    }
-})
-
-app.post('/videos', (req: Request, res: Response) => {
-    let title = req.body.title
-    if (!title || typeof title !== 'string' || !title.trim() || title.length > 40) {
-        res.status(400).send({
-            errorsMessages: [
-                {
-                    'message': 'Incorrect title',
-                    'field': 'title'
-                }
-            ]//,
-            // "resultCode": 1
-        })
-        return
-    }
-    const newVideo = {
-        id: +(new Date()),
-        //id: Math.floor(Math.random() * 10000),
-        title: req.body.title,
-        author: 'it-incubator.eu'
-        //author: req.body.author
-    }
-    videos.push(newVideo)
-    res.status(201).send(newVideo)
-})
-
-app.delete('/videos/:videoId', (req: Request, res: Response) => {
-    //videos = videos.filter((y) => y.id !== +req.params.id)
-    //res.send(204)
-    const id = +req.params.videoId;
-    if (!id) {
-        res.sendStatus(404)
-    } else {
-        let video = videos.find(y => y.id === id)
-        if (!!video) {
-            //res.send(video)
-            videos = videos.filter((y) => y.id !== id)
-            res.sendStatus(204)
-        } else {
-            res.sendStatus(404)
-        }
-    }
-})
-
-app.put('/videos/:id', (req: Request, res: Response) => {
-    let title = req.body.title
-    if (!title || typeof title !== 'string' || !title.trim() || title.length > 40) {
-        res.status(400).send({
-            errorsMessages: [
-                {
-                    'message': 'Incorrect title',
-                    'field': 'title'
-                }
-            ]//,
-            // "resultCode": 1
-        })
-        return
-    }
-    const id = +req.params.id;
-    if (!id) {
-        res.sendStatus(404)
-
-        return;
-    }
-
-    let video = videos.find(v => v.id === id)
-    if (!!video) {
-        video.title = req.body.title
-        res.sendStatus(204)
-
-        return;
-    }
-
-    res.sendStatus(404)
-})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
